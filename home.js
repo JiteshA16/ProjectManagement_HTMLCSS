378239d91515a0791903bd5b75b7e931e0d6e032
Jitesh <jitesh.acharya2400@gmail.com>
2021-02-01 03:18:40 +0530

    Added the task page files and made changes in home files

--- home.js
+++ home.js
@@ -1,6 +1,4 @@
-
-var projectListData = [
-    {
+var projectListData = [{
         name: 'Javascript One'
     },
     {
@@ -8,18 +6,18 @@ var projectListData = [
     }
 ];
 
-showProjects();
+//showProjects();
 
 function showProjects(listId) {
-    projectListData.forEach(function (value, index) {
+    projectListData.forEach(function(value, index) {
         var template =
-            '<div class="project-card">'
-            + value.name
-            + '<ul>'
-            + '<li>Task one</li>'
-            + '<li>Task two</li>'
-            + '</ul>'
-            + '</div>';
+            '<div class="project-card">' +
+            value.name +
+            '<ul>' +
+            '<li>Task one</li>' +
+            '<li>Task two</li>' +
+            '</ul>' +
+            '</div>';
         if (listId) {
             document.getElementById(listId).innerHTML += template;
         } else {
@@ -39,22 +37,35 @@ function addBoard(id) {
     if (boardTitle == "") {
         alert('Board title cannot be empty.');
     } else {
+        // var alertText = 'Board named ' + boardTitle + ' is added.'
+        // alert(alertText);
         idValue++;
         var projectListUniqueId = "projectList_" + idValue;
-        console.log(idValue)
+        var boardUniqueId = "board_" + idValue;
         var templateBlock =
-            '<section class="board-block">'
-            + '<div>'
-            + boardTitle
-            + '<button style="margin-left: 5px;" onclick="removeProjects()">Remove projects</button>'
-            + '</div>'
-            + '<div class="project-container" id=' + projectListUniqueId + '>'
-            + '</div>'
-            + '</section>';
+            '<section class="board-block" id=' + boardUniqueId + '>' +
+            '<div class="board-block-header">' +
+            boardTitle +
+            '<button style="background-color: red; color: white; border: none; height: 30px; border-radius: 5px;" onclick="removeProjects()">Remove projects</button>' +
+            '</div>' +
+            '<div class="project-container" id=' + projectListUniqueId + '>' +
+            '</div>' +
+            '</section>';
         document.getElementById('boardBlockList').innerHTML += templateBlock
         document.getElementById('newBoardTitleText').value = '';
         showProjects(projectListUniqueId);
+        document.getElementById('menuList').innerHTML += '<li onclick="openProject(' + boardUniqueId + ')" id="menuItems">' + boardTitle + '</li>';
+        document.getElementById('menuList').innerHTML += '<div style="height:1px; background-color: white;"></div>';
     }
+}
+
+function openProject(projectBoard) {
+    console.log(projectBoard);
+    var projectBoards = document.getElementById('boardBlockList').getElementsByClassName('board-block');
+    for (var i = 0; i < projectBoards.length; i++) {
+        document.getElementById(projectBoards[i].id).style.display = 'none';
+    }
+    document.getElementById(projectBoard.id).style.display = 'block';
 }
 
 
